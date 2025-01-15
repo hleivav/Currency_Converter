6894989612965c1c55e8ab6dd482416f83ba712d
Hernan Leiva <hleiva@hotmail.com>
2025-01-15 15:12:05 +0100

    functionality working. remains validations

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -1,4 +1,7 @@
 package org.example;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
 
 public class Converter {
     // Static class that handles the convertion operations.
@@ -13,31 +16,69 @@ public class Converter {
         public static void appStart() {
             //initiates a loop that keeps the program running until the user ends it.
             print("Welcome to the currency converter!");
-            while (done){
+            while (!done){
                 menuCreator();
                 choiceHandler();
-
             }
         }
 
         public static void menuCreator() {
             //creates and print a menu that will be called every time the user needs a new convertion.
-
+            //print("Det ger: " +  makeTheConversion(SEK, "USD", 400));
+            String [] optionsArray = { "Exit", "SEK to USD", "USD to SEK", "SEK to EURO", "EURO to SEK"};
+            print("Choose the number of the operation you need.");
+            for (int i = 1; i < optionsArray.length; i++) {
+                print( i + ". Convert " + optionsArray[i]);
+            }
+            print("0. " + optionsArray[0]);
         }
 
         public static void choiceHandler() {
             //Handle the user choices to convert to the different currencies.
+
+            Scanner scanner = new Scanner(System.in);
+            String userChoice = scanner.nextLine();
+            if (!userChoice.equals("0")){
+                print("What is the amount to convert?");
+                int amountToConvert = Integer.parseInt(scanner.nextLine());
+                double result = 0;
+                switch (userChoice){
+                    case "1":
+                        makeTheConversion(SEK, "USD", amountToConvert);
+                        break;
+                    case "2":
+                        makeTheConversion(USD, "SEK", amountToConvert);
+                        break;
+                    case "3":
+                        makeTheConversion(SEK, "EUR", amountToConvert);
+                        break;
+                    case "4":
+                        makeTheConversion(EUR, "SEK", amountToConvert);
+                        break;
+                    default:
+                        print("invalid choice!");
+                        break;
+                }
+                print("Do you need a new convertion? Y/N");
+            } else {
+                print("thanks by using our program and welcome back!");
+                done = true;
+            }
+
         }
 
-        public static double makeTheConversion(Currency currency, String convertTo, int amount) {
+        public static void makeTheConversion(Currency currency, String convertTo, int amount) {
             //take an object to convert, a String to find and an amount.
+
             double result = 0;
             if ( convertTo.equalsIgnoreCase(currency.getCurrency1())){
                 result = amount * currency.getExchangeRate1();
             }else {
                 result = amount * currency.getExchangeRate2();
             }
-            return result;
+            BigDecimal bd = new BigDecimal(result);
+            bd = bd.setScale(2, RoundingMode.HALF_UP);
+            print("That is " + bd.doubleValue() + " in " + convertTo);
         }
 
         public static void print(String stringToPrint){
--- src/main/java/org/example/Currency.java
+++ src/main/java/org/example/Currency.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Currency {
-    private String currency1;
-    private double exchangeRate1;
-    private String currency2;
-    private double exchangeRate2;
+    private final String currency1;
+    private final double exchangeRate1;
+    private final String currency2;
+    private final double exchangeRate2;
 
     //Constructor
     public Currency (String currency1, double exchangeRate1, String currency2, double exchangeRate2){
@@ -16,11 +16,6 @@ public class Currency {
     //Getters
     public String getCurrency1(){return currency1; }
     public double getExchangeRate1(){return exchangeRate1;}
-    public String getCurrency2(){return currency2;}
+    //public String getCurrency2(){return currency2;}
     public double getExchangeRate2(){return exchangeRate2;}
-    //Setters won't be used
-    /*public void setCurrency1(String currency){this.currency1 = currency;}
-    public void setExchangeRate1(double exchangeRate){this.exchangeRate1 = exchangeRate;}
-    public void setCurrency2(String currency){this.currency2 = currency;}
-    public void setExchangeRate2(double exchangeRate){this.exchangeRate2 = exchangeRate;}*/
 }
