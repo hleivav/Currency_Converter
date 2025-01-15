6d5fa08b675a2b859b2ff769579594a887f1386e
Hernan Leiva <hleiva@hotmail.com>
2025-01-15 11:40:20 +0100

    structure completed

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -2,10 +2,22 @@ package org.example;
 
 public class Converter {
     // Static class that handles the convertion operations.
+
     public static class ConverterHelp {
+        static boolean done = false;
+        //create currency objects with the three currencies.
+        static Currency SEK = new Currency("USD", 0.0896, "EUR", 0.0869);
+        static Currency USD = new Currency("SEK", 11.1549, "EUR", 0.9699);
+        static Currency EUR = new Currency("SEK", 11.501, "USD", 1.031);
 
         public static void appStart() {
-            //initiates a loop who keeps the program running until the user ends it.
+            //initiates a loop that keeps the program running until the user ends it.
+            print("Welcome to the currency converter!");
+            while (done){
+                menuCreator();
+                choiceHandler();
+
+            }
         }
 
         public static void menuCreator() {
@@ -17,8 +29,14 @@ public class Converter {
             //Handle the user choices to convert to the different currencies.
         }
 
-        public static double makeTheConversion(String currency, double currencyValue, int amount) {
-            return currencyValue * amount;
+        public static double makeTheConversion(Currency currency, String convertTo, int amount) {
+            double result = 0;
+            if ( convertTo.equalsIgnoreCase(currency.getCurrency1())){
+                result = amount * currency.getExchangeRate1();
+            }else {
+                result = amount * currency.getExchangeRate2();
+            }
+            return result;
         }
 
         public static void print(String stringToPrint){
--- src/main/java/org/example/Currency.java
+++ src/main/java/org/example/Currency.java
@@ -1,16 +1,26 @@
 package org.example;
 
 public class Currency {
-    private final String name;
-    private final double currency;
+    private String currency1;
+    private double exchangeRate1;
+    private String currency2;
+    private double exchangeRate2;
 
     //Constructor
-    public Currency (String name, double currency){
-        this.name = name;
-        this.currency = currency;
+    public Currency (String currency1, double exchangeRate1, String currency2, double exchangeRate2){
+        this.currency1 = currency1;
+        this.exchangeRate1 = exchangeRate1;
+        this.currency2 = currency2;
+        this.exchangeRate2 = exchangeRate2;
     }
     //Getters
-    public String getName(){return name; }
-
-    public double getCurrency() { return currency;}
+    public String getCurrency1(){return currency1; }
+    public double getExchangeRate1(){return exchangeRate1;}
+    public String getCurrency2(){return currency2;}
+    public double getExchangeRate2(){return exchangeRate2;}
+    //Setters
+    public void setCurrency1(String currency){this.currency1 = currency;}
+    public void setExchangeRate1(double exchangeRate){this.exchangeRate1 = exchangeRate;}
+    public void setCurrency2(String currency){this.currency2 = currency;}
+    public void setExchangeRate2(double exchangeRate){this.exchangeRate2 = exchangeRate;}
 }
